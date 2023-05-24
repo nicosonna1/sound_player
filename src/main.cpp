6978f09c6717026e3cc638b2c032ef2efc8206de
Nicolas Sonnard <114679645+nicosonna1@users.noreply.github.com>
2023-05-24 19:25:57 +0200

    modif

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,21 @@
 #include <Arduino.h>
-
-/*!
- *@file play.ino
- *@brief Music Playing Example Program
- *@details  Experimental phenomenon: control MP3 play music, obtain song information
- *@copyright  Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
- *@license     The MIT license (MIT)
- *@author [fengli](li.feng@dfrobot.com)
- *@version  V1.1
- *@date  2021-10-15
- *@url https://github.com/DFRobot/DFRobot_DF1201S
-*/
-
-
 #include <DFRobot_DF1201S.h>
 
+
+// define
+
+#define BT_PLAY 5
 
 
 DFRobot_DF1201S DF1201S;
 void setup(void){
+
+  // initialisation du serial pour le debug cote pc
   Serial.begin(115200);
+  // initilisation du serial pour le modul son
   Serial2.begin(115200);
+  pinMode(BT_PLAY,INPUT_PULLUP);
+
   while(!DF1201S.begin(Serial2)){
     Serial.println("Init failed, please check the wire connection!");
     delay(1000);
@@ -34,11 +29,7 @@ void setup(void){
   DF1201S.switchFunction(DF1201S.MUSIC);
   /*Wait for the end of the prompt tone */
   delay(2000);
-  /*Set playback mode to "repeat all"*/
-  DF1201S.setPlayMode(DF1201S.ALLCYCLE);
-  Serial.print("PlayMode:");
-  /*Get playback mode*/
-  Serial.println(DF1201S.getPlayMode());
+
   
   //Set baud rate to 115200(Need to power off and restart, power-down save)
   //DF1201S.setBaudRate(115200);
@@ -53,55 +44,14 @@ void setup(void){
 }
 
 void loop(){
+
+  // lecture du switch
+  if (digitalRead(BT_PLAY)==LOW){
   Serial.println("Start playing");
   /*Start playing*/
   DF1201S.start();
-  delay(3000);
-  Serial.println("Pause");
-  /*Pause*/
-  DF1201S.pause();
-  delay(3000);
-  Serial.println("Next");
-  /*Play the next song*/
-  DF1201S.next();
-  delay(3000);
-  Serial.println("Previous");
-  /*Play the previous song*/
-  DF1201S.last();
-  delay(3000);
-  Serial.println("Start playing");
-  //Fast forward 10S
-  DF1201S.fastForward(/*FF = */10);
-  //Fast Rewind 10S
-  //DF1201S.fastReverse(/*FR = */10);
-  //Start the song from the 10th second 
-  //DF1201S.setPlayTime(/*Play Time = */10);
-  
-  Serial.print("File number:");
-  //Get file number
-  Serial.println(DF1201S.getCurFileNumber());
-  
-  Serial.print("The number of files available to play:");
-  //The number of files available to play
-  Serial.println(DF1201S.getTotalFile());
-  
-  Serial.print("The time length the current song has played:");
-  //Get the time length the current song has played 
-  Serial.println(DF1201S.getCurTime());
-  
-  Serial.print("The total length of the currently-playing song: ");
-  //Get the total length of the currently-playing song 
-  Serial.println(DF1201S.getTotalTime());
-  Serial.print("The name of the currently-playing file: ");
-  //Get the name of the playing file 
-  Serial.println(DF1201S.getFileName());
-  delay(3000);
-  //Play the file No.1, the numbers are arranged according to the sequence of the files copied into the U-disk 
-  DF1201S.playFileNum(/*File Number = */1);
-  //Play the test.mp3 file in test folder 
-  //DF1201S.playSpecFile("/test/test.mp3");
-  
-  while(1);
-  /*Delete the currently-playing file */
-  //DF1201S.delCurFile();
+  // ceci est un anti rebon pour le bouton 
+  delay(150);
+  }
+
 }
